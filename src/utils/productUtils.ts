33c56566974b730cf70d3657fef653721c45175d
ksookdhew <ksookdhew@jhb.dvt.co.za>
2024-08-07 17:27:09 +0200

    fix: Rounding error
    Fixed the rounding error to round the price to 2 decimal places

--- src/utils/productUtils.ts
+++ src/utils/productUtils.ts
@@ -1,3 +1,3 @@
 export const formattedPrice = (value: number) => {
-    return `$ ${((Math.round(value) * 100) / 100).toFixed(2)}`;
-};
+    return `$ ${((Math.round(value * 100) )/ 100).toFixed(2)}`;
+}
