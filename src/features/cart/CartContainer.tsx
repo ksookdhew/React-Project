e32d3f36d25d81b5a75ef267acd06c9a1c9e0e25
ksookdhew <ksookdhew@jhb.dvt.co.za>
2024-08-08 11:10:07 +0200

    feat: clear cart

--- src/features/cart/CartContainer.tsx
+++ src/features/cart/CartContainer.tsx
@@ -5,10 +5,12 @@ import {useQuery} from "@tanstack/react-query";
 import {Product} from "../../models/Products.ts";
 import {getAllProducts} from "../../services/api.ts";
 import {formattedPrice} from "../../utils/productUtils.ts";
+import {Link} from "react-router-dom";
 
 const CartContainer = () => {
     const cartState = useCartStore((state) => state.cart);
     const totalNumItems = useCartStore((state) => state.cartNumItems);
+    const clearCart = useCartStore((state) => state.clearCart);
     const allProductsQuery = useQuery<Product[]>({
         queryKey: ['allProducts'],
         queryFn: getAllProducts
@@ -24,13 +26,21 @@ const CartContainer = () => {
         <div className="p-4">
             <h1 className="text-4xl">Your Shopping Cart</h1>
             {cartState.length === 0 ? (
-                <div>Empty Cart</div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Your cart is empty</h2>
+                        <Link to="/">
+                            <button className="btn btn-primary">Continue shopping</button>
+                        </Link>
+                    </div>
+                </div>
             ) : (
                 <div className="flex flex-wrap w-full shadow-xl h-full mr-2 mt-2">
-                    <div className="w-full md:w-2/3 flex flex-col self-center gap-8 px-6 ">
+                    <div className="w-full md:w-2/3 flex flex-col self-center justify-start gap-8 px-6 ">
                         {cartState.map((item) => (
                             <CartProductCard key={item.productId} cartItem={item}/>
                         ))}
+                        <button className="btn btn-neutral w-fit" onClick={clearCart}>Clear cart</button>
                     </div>
                     <div className="w-full md:w-1/3">
                         <CartSummary total={formattedPrice(totalPrice)} numItems={totalNumItems}/>
