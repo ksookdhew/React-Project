75c1abf8235e5a8b8e8cf6aac525e7a6adebd667
ksookdhew <ksookdhew@jhb.dvt.co.za>
2024-08-08 09:03:14 +0200

    feat: Added cart summary card

--- src/features/cart/CartContainer.tsx
+++ src/features/cart/CartContainer.tsx
@@ -1,19 +1,44 @@
-import { useCartStore } from "./cartStore.ts";
+import {useCartStore} from "./cartStore.ts";
 import CartProductCard from "./CartProductCard.tsx";
+import CartSummary from "./CartSummary.tsx";
+import {useQuery} from "@tanstack/react-query";
+import {Product} from "../../models/Products.ts";
+import {getAllProducts} from "../../services/api.ts";
+import {formattedPrice} from "../../utils/productUtils.ts";
 
 const CartContainer = () => {
     const cartState = useCartStore((state) => state.cart);
+    const allProductsQuery = useQuery<Product[]>({
+        queryKey: ['allProducts'],
+        queryFn: getAllProducts
+    });
 
-    console.log(cartState)
+    const totalPrice = cartState.reduce((runningTotal, item) => {
+        const product = allProductsQuery.data?.find((prod) => prod.id === item.productId);
+        const productPrice = product?.price ?? 0;
+        return runningTotal + productPrice * item.quantity;
+    }, 0);
+
+    const totalNumItems = cartState.reduce((runningTotal, item) => {
+        return runningTotal + item.quantity;
+    }, 0);
 
     return (
-        <div className=" flex flex-wrap gap-4 p-4">
+        <div className="p-4">
+            <h1 className="text-4xl">Your Shopping Cart</h1>
             {cartState.length === 0 ? (
                 <div>Empty Cart</div>
             ) : (
-                cartState.map((item) => (
-                    <CartProductCard key={item.productId} cartItem={item} />
-                ))
+                <div className="flex flex-wrap w-full shadow-xl h-full mr-2 mt-2">
+                    <div className="w-full md:w-2/3 flex flex-col self-center gap-8 px-6 ">
+                        {cartState.map((item) => (
+                            <CartProductCard key={item.productId} cartItem={item}/>
+                        ))}
+                    </div>
+                    <div className="w-full md:w-1/3">
+                        <CartSummary total={formattedPrice(totalPrice)} numItems={totalNumItems}/>
+                    </div>
+                </div>
             )}
         </div>
     );
--- src/features/cart/CartProductCard.tsx
+++ src/features/cart/CartProductCard.tsx
@@ -1,11 +1,13 @@
-import {CartItem} from "./cartStore.ts";
+import {CartItem, useCartStore} from "./cartStore.ts";
 import {useQuery} from "@tanstack/react-query";
 import {getProductWithId} from "../../services/api.ts";
 import {formattedPrice} from "../../utils/productUtils.ts";
 
 const CartProductCard = ({cartItem}: {cartItem:CartItem}) => {
     const productQuery = useQuery({queryKey: [cartItem.productId], queryFn: () => getProductWithId(`${cartItem.productId}` ?? '')})
-
+    const addItemToCart = useCartStore((state) => state.addItem);
+    const decreaseItemFromCart = useCartStore((state) => state.decreaseItem);
+    const removeItemFromCart = useCartStore((state) => state.removeFromCart);
 
     if (productQuery.isPending) return <div>Loading...</div>
 
@@ -13,7 +15,7 @@ const CartProductCard = ({cartItem}: {cartItem:CartItem}) => {
 
     const product = productQuery.data
     return(
-        <div className="card card-side bg-base-100 shadow-xl w-full md:w-1/2">
+        <div className="card card-side bg-base-100 shadow-xl w-full">
             <figure className="w-32">
                 <img
                     className={"w-full"}
@@ -21,12 +23,28 @@ const CartProductCard = ({cartItem}: {cartItem:CartItem}) => {
                     alt="Product"/>
             </figure>
             <div className="card-body">
+                <div className="card-actions justify-end">
+                    <button className="btn btn-square btn-sm" onClick={() => removeItemFromCart(product?.id)}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"/>
+                        </svg>
+                    </button>
+                </div>
                 <h2 className="card-title">{product?.title}</h2>
                 <p>{formattedPrice(product?.price)}</p>
                 <div className="card-actions justify-end w-fit">
-                    <button className="btn btn-primary btn-sm" >-</button>
+                    <button className="btn btn-primary btn-sm" onClick={() => decreaseItemFromCart(product?.id)}>-</button>
                     <p className="self-center text-2xl">{cartItem.quantity} </p>
-                    <button className="btn btn-primary btn-sm">+</button>
+                    <button className="btn btn-primary btn-sm" onClick={() => addItemToCart(product?.id)}>+</button>
                 </div>
             </div>
         </div>
