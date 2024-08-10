774e080cbdd2926f7b7d16233cc918dca2bcafe0
ksookdhew <ksookdhew@jhb.dvt.co.za>
2024-08-10 14:11:26 +0200

    feat: Cart Item loader
    Created the skeleton loader for the Cart items

--- src/features/cart/CartProductCard.tsx
+++ src/features/cart/CartProductCard.tsx
@@ -2,6 +2,7 @@ import {CartItem, useCartStore} from "./cartStore.ts";
 import {useQuery} from "@tanstack/react-query";
 import {getProductWithId} from "../../services/api.ts";
 import {formattedPrice} from "../../utils/productUtils.ts";
+import CartItemLoader from "./CartItemLoader.tsx";
 
 const CartProductCard = ({cartItem}: { cartItem: CartItem }) => {
     const productQuery = useQuery({
@@ -12,7 +13,7 @@ const CartProductCard = ({cartItem}: { cartItem: CartItem }) => {
     const decreaseItemFromCart = useCartStore((state) => state.decreaseItem);
     const removeItemFromCart = useCartStore((state) => state.removeFromCart);
 
-    if (productQuery.isPending) return <div>Loading...</div>
+    if (productQuery.isPending) return <CartItemLoader/>
 
     if (productQuery.error) return <div>Error: {productQuery.error.message}</div>
 
